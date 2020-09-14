5ce2916736bae82ef204dfca7c15d4d0ab23f242
Federico Marroquin <f.marroquin10@uniandes.edu.co>
2020-09-14 01:18:43 -0500

    Navigation 3 full

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
+import { Card} from 'react-native-elements';
+import { DISHES} from '../shared/dishes';
+import { PROMOTIONS} from '../shared/promotions';
+import { LEADERS} from '../shared/leaders';
 
+
+function RenterItem(props) {
+    const item = props.item;
+    if(item!= null){
+        return(
+            <Card>
+                <Card.Image style= {{ alignItems: 'center', justifyContent: 'center' }} source= {require('./images/uthappizza.png')}>
+                    <Card.FeaturedTitle >{item.name}</Card.FeaturedTitle>
+                    <Card.FeaturedSubtitle>{item.designation}</Card.FeaturedSubtitle>
+                </Card.Image>
+                <Text style={{margin: 10}}>
+                    {item.description}
+                </Text>
+            </Card>
+            
+        );
+    }
+    else{
+        return(<View></View>)
+    }
+}
 class Home extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            dishes: DISHES,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
+        }
+    }
      
     render(){
         return (
-            <View>
-                <Text>Home Component</Text>
-            </View>
+            <ScrollView>
+                <RenterItem item={ this.state.dishes.filter((dish) => dish.featured)[0]}/>
+                <RenterItem item={ this.state.promotions.filter((promo) => promo.featured)[0]}/>
+                <RenterItem item={ this.state.leaders.filter((leader) => leader.featured)[0]}/>
+            </ScrollView>
         );
     }
 }
